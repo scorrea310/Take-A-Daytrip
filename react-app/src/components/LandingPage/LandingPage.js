46c6c693a17d1a3a6d5568aa9926cdf1c8287e10
Steve Correa <s.correa@berkeley.edu>
2022-02-15 10:52:30 -0800

    made basic navbar structure. working on profile button modal window in click.

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
 
     return (
         <div className="landingPage">
-            <NavBar />
+            <NavBar landingPage={true} />
         </div>
     )
 }
--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,14 +1,47 @@
 import "./NavBar.css"
+import { BiMenu } from "react-icons/bi"
+import { FaRegUserCircle } from "react-icons/fa"
+import { useState } from "react"
+
+const NavBar = ({ landingPage }) => {
+
+    const [profileModal, setProfileModal] = useState(false)
+
+    // const searchStay = (
+    //     <div className="searchStayContainer">
+    //         <div class="searchStayForm"> Hello
+    //         </div>
+    //     </div>
+    // )
 
 
-const NavBar = () => {
+
+
 
     return (
-        <div className="NavBarContainer">
-            <div className="navLogoContainer">
-                <div id="navLogo"></div>
+        <>
+            <div className="NavBarContainer">
+                <div className="centerNavBarContainer">
+                    <div className="navLogoContainer">
+                        <div id="navLogo" ></div>
+                        <div className="takeADayTripText">Take A Daytrip</div>
+                    </div>
+                    <div className="placesToStayTextContainer"> Places to Stay
+                        {/* <div id="placesToStayBottomBorder"></div> */}
+                    </div>
+                    <div className="profileButtonContainer">
+                        <div className="navBarProfileButton" onClick={() => setProfileModal(true)}>
+                            <BiMenu style={{ fontSize: "22px", marginRight: "5px" }} />
+                            <FaRegUserCircle style={{ fontSize: "22px" }} />
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
-        </div>
+            {profileModal ? <div style={{ marginTop: "100px" }}>hello</div> : null}
+            {/* {landingPage ? searchStay : null} */}
+        </>
     )
 }
 
