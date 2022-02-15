335db1f095bd9f83246e891fb1bbcbd6a32605d3
Steve Correa <s.correa@berkeley.edu>
2022-02-14 22:27:57 -0800

    added LandingPage and Navbar components and simple structure.

---
+++ react-app/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import "./NavBar.css"
+
+
+const NavBar = () => {
+
+    return (
+        <div className="NavBarContainer">
+            <div className="navLogoContainer">
+                <div id="navLogo"></div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
