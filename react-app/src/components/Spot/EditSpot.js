31fbc4566f6d0f84916d7e9ee43ab6c874522c0b
Steve Correa <s.correa@berkeley.edu>
2022-02-20 15:04:49 -0800

    working on edit modal.

--- react-app/src/components/Spot/EditSpot.js
+++ react-app/src/components/Spot/EditSpot.js
@@ -1,9 +1,28 @@
-
+import "./EditSpot.css"
+import { AiFillEdit } from "react-icons/ai"
+import { AiFillDelete } from "react-icons/ai"
+import { Modal } from '../../context/Modal';
+import { useState, useEffect } from "react"
+import { IoMdClose } from "react-icons/io"
 
 const EditSpot = () => {
-
+    const [showEditModal, setShowEditModal] = useState(false);
     return (
-        <div>EDit</div>
+        <div className="editSpotContainer">
+            <div className="editDaytripText">Edit/Delete Daytrip</div>
+            <div className="editOrDeleteSpotContainer">
+                <div className="editIconAndText" onClick={() => setShowEditModal(true)}><AiFillEdit id="editIcon" /> Edit Daytrip</div>
+                <div className="deleteIconAndText"><AiFillDelete id="deleteIcon" /> Delete Daytrip</div>
+            </div>
+            {showEditModal && <Modal idName="editModal" onClose={() => setShowEditModal(false)}>
+                <div className="editModalMainContent">
+                    <div className="closeIconAndEditTextContainer">
+                        <div></div>
+                        <IoMdClose className="closeLoginModalIcon" />
+                    </div>
+                </div>
+            </Modal>}
+        </div>
     )
 }
 
