dace7cd72ab1acae0f66f246bbb5686280d7e0f4
Steve Correa <s.correa@berkeley.edu>
2022-02-20 12:45:20 -0800

    added styling and structure for spot page except for edit/reserve section and the footer.

--- react-app/src/components/Spot/Spot.js
+++ react-app/src/components/Spot/Spot.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import "./Spot.css"
+import { BsDoorOpen } from "react-icons/bs"
+import { BsCalendarDate } from "react-icons/bs"
 
 
 const Spot = ({ sessionId, spotsLoaded }) => {
@@ -17,6 +19,12 @@ const Spot = ({ sessionId, spotsLoaded }) => {
     style={{ width: "100%", height: "357px", backgroundImage: `url(${spot[`${spotId}`]?.images[0]})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderRadius: "12px" }}
     
     */
+
+    /*
+    total_bathrooms: "0"
+    total_bedrooms: "0"
+    total_occupancy: "0"
+    */
     let spotDiv = (
         <div className='spotPageContainer'>
             <div className='spotPageNavBArContainerParent'>
@@ -27,7 +35,25 @@ const Spot = ({ sessionId, spotsLoaded }) => {
                 <div className='spotPageImagesSection'>
                     <div style={{ width: "49%", height: "357px", backgroundImage: `url(${spot[`${spotId}`]?.images[0]})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderRadius: "12px" }}></div>
                     <div style={{ width: "49%", height: "357px", backgroundImage: `url(${spot[`${spotId}`]?.images[1]})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderRadius: "12px" }}></div>
-
+                </div>
+            </div>
+            <div className='spotInfoAndReserveOrEditContainer'>
+                <div className='spotDescriptionAndInfoDiv'>
+                    <div className='spotDetailsTitleAndStats'>
+                        <h2>{spot[`${spotId}`]?.type} hosted by {spot[`${spotId}`]?.host_name}</h2>
+                        <div className='spotStatContainer'>
+                            Total Bathrooms {spot[`${spotId}`]?.total_bathrooms} - Total Bedrooms {spot[`${spotId}`]?.total_bedrooms} - Total Occupants Allowed {spot[`${spotId}`]?.total_occupancy}
+                        </div>
+                    </div>
+                    <div className='reservationAndCancelationTextContainer'>
+                        <div className='doorAndSelfTextContainer'> <BsDoorOpen id="doorIcon" /> Self Check In </div>
+                        <div className='checkInYouselfText'>Check yourself by showing up.</div>
+                        <div className='calendarAndCancelationText'> <BsCalendarDate id="calendarIcon" /> Free cancellation before start date. </div>
+                    </div>
+                    <div className='spotDescriptionContainer'>{spot[`${spotId}`]?.description}</div>
+                </div>
+                <div className='editDeleteOrReserveSection'>
+                    
                 </div>
             </div>
         </div>
