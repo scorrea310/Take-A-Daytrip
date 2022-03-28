f542bb1316e24c6e2ec6cf2f8e97ab3165a2c6a7
Steve Correa <s.correa@berkeley.edu>
2022-03-27 19:59:22 -0700

    finished places you've been page.

--- react-app/src/components/ManageListings/ListingCard.js
+++ react-app/src/components/ManageListings/ListingCard.js
@@ -2,19 +2,26 @@ import "./ManageListings.css"
 import { useHistory } from "react-router-dom"
 
 
-const ListingCard = ({ listing }) => {
+const ListingCard = ({ listing, PastTrip }) => {
 
     const history = useHistory()
 
     return (
-        <div className="listingCard" onClick={() => history.push(`/spots/${listing.id}`)}>
+        <div className="listingCard" onClick={() => {
+            if (listing) {
+                history.push(`/spots/${listing.id}`)
+            } else if (PastTrip) {
+                history.push(`/spots/${PastTrip.spot_id}`)
+            }
+
+        }}>
             {/* <div id="listingNameListingCard">{listing.name}</div> */}
             <div>
-                <img id="yourListingImageCard" alt="your-listing" src={listing.images[0]}></img>
+                <img id="yourListingImageCard" alt="your-listing" src={listing ? listing.images[0] : PastTrip.spot_images[0]}></img>
             </div>
             <div className="listingNameAndPriceContainer">
-                <div id="listingAddress">{listing.address}</div>
-                <div id="listingPriceYourListings">${listing.price_per_day}/ day</div>
+                <div id="listingAddress">{listing ? listing.address : PastTrip.spot_address}</div>
+                <div id="listingPriceYourListings">${listing ? listing.price_per_day : PastTrip.price}/ day</div>
             </div>
         </div>
     )
