5c481bd63592cd1705b6650084163adc96530e1b
Steve Correa <s.correa@berkeley.edu>
2022-04-13 22:41:20 -0700

    set up icons for mobile nav bar when signed out.

--- react-app/src/components/NavBar/MobileNav.js
+++ react-app/src/components/NavBar/MobileNav.js
@@ -1,12 +1,35 @@
 import "./MobileNav.css"
-
-
+import { useSelector } from 'react-redux';
+import { BiUserCircle } from "react-icons/bi"
+import { MdEmojiPeople } from "react-icons/md"
+import { MdOutlineAppRegistration } from "react-icons/md"
 
 
 const MobileNav = () => {
 
+    const session = useSelector((state) => state.session.user);
+
+    const signedOutOptions = (
+        <div className="signedOutMobileNavMainContent">
+            <div className="mobileNavUnit demo">
+                <MdEmojiPeople id="demoUserIcon" />
+                <div className="mobileNavUnitText">Demo User</div>
+            </div>
+            <div className="mobileNavUnit login">
+                <BiUserCircle id="loginIconMobileNav" />
+                <div className="mobileNavUnitText">Log In</div>
+            </div>
+            <div className="mobileNavUnit register">
+                <MdOutlineAppRegistration id="registerIconMobileNav" />
+                <div className="mobileNavUnitText">Register</div>
+            </div>
+        </div>
+    )
+
     return (
-        <div className="mobileNav"></div>
+        <div className="mobileNav">
+            {!session && signedOutOptions}
+        </div>
     )
 }
 
