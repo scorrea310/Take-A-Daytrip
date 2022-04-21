0afab199fc6a2a9d1bdc5b1162f4766d6dbabdaf
Steve Correa <s.correa@berkeley.edu>
2022-04-20 23:18:26 -0700

    added additional tests for reservation routes.

--- tests/test_reservation_routes.py
+++ tests/test_reservation_routes.py
@@ -1,6 +1,37 @@
 import pytest
 from app import app 
 
-def test_get_reservations_for_user():
+def test_get_reservations_for_user_status_code():
+    """
+    This function tests the status code for the get
+    users reservations api route.
+    """ 
     response = app.test_client().get('/api/reservations/1')
     assert response.status_code == 201
+
+def test_get_reservation_for_user_response():
+    """
+    This function tests the response data for the get
+    reservation for user api route.
+    """ 
+    pass
+
+def test_post_new_reservation():
+    """
+    This tests the post reservation ap route.
+    """ 
+    pass
+
+def test_patch_reservation():
+    """
+    this tests editing an existing reservation.
+    """ 
+    pass
+
+
+
+
+
+
+
+
