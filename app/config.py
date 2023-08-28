9b22fd5d74f7b0e7be4739cfcf940a09fd06a232
Steve Correa <s.correa@berkeley.edu>
2023-08-28 13:09:02 -0700

    edited flask app config to work in development.

--- app/config.py
+++ app/config.py
@@ -2,14 +2,7 @@ import os
 import click
 import json
 
-db_secret_json = os.environ.get("DATABASE_URL")
-parsed_secret = json.loads(db_secret_json)
-db_connection_string = (
-    f"{parsed_secret['engine']}://"
-    f"{parsed_secret['username']}:{parsed_secret['password']}@"
-    f"{parsed_secret['host']}:{parsed_secret['port']}/"
-    f"{parsed_secret['dbname']}"
-)
+db_secret = os.environ.get("DATABASE_URL")
 
 
 class Config:
@@ -19,11 +12,21 @@ class Config:
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here
-    SQLALCHEMY_DATABASE_URI = db_connection_string.replace(
-        "postgres://", "postgresql://"
-    )
     SQLALCHEMY_ECHO = True
     GOOGLE_API_KEY = os.environ.get("MAPS_API_KEY")
     LOB_ADDRESS_VERIFICATION_API_KEY = os.environ.get(
         "LOB_ADDRESS_VERIFICATION_API_KEY"
     )
+    if os.environ.get("FLASK_ENV") == "production":
+        parsed_secret = json.loads(db_secret)
+        db_connection_string = (
+            f"{parsed_secret['engine']}://"
+            f"{parsed_secret['username']}:{parsed_secret['password']}@"
+            f"{parsed_secret['host']}:{parsed_secret['port']}/"
+            f"{parsed_secret['dbname']}"
+        )
+        SQLALCHEMY_DATABASE_URI = db_connection_string.replace(
+            "postgres://", "postgresql://"
+        )
+    else:
+        SQLALCHEMY_DATABASE_URI = db_secret
